e9e6e2c2945e4b2b64f9c17c89f01f89ccb769b2
johngarcia9110 <johngarcia9110@gmail.com>
2017-02-01 13:00:45 -0500

    added premetive form validation before sendoff, style changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
+    console.log('loaded');
+    $("#submit-button").click(function(event){
+      
+      var name = $('#inf_field_FirstName').val();
+      var company = $('#inf_custom_Company0').val();
+      var phone = $('#inf_field_Phone1').val();
+      var email = $('#inf_field_Email').val();
+      
+      if(
+        name === '' || company === '' || phone === '' || email === ''
+      ){
+        event.preventDefault();
+        $('#alertMsg').show();
+        console.log('no info submited');
+      }else{
+        $('#mitForm').submit();
+      }
+    });
 });
